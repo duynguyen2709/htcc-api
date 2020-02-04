7fac0b18e803bae0e457278a40da78ed5daf8c2d
duyna5 <duyna5@vng.com.vn>
2020-02-04 16:51:16 +0700

    init config service

---
+++ htcc-config-service/src/main/java/vn/edu/hcmus/htccconfigservice/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package vn.edu.hcmus.htccconfigservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@EnableWebSecurity
+@Component
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.user.name}")
+    private String username;
+
+    @Value("${security.user.name}")
+    private String password;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(username)
+                .password(password)
+                .authorities("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
