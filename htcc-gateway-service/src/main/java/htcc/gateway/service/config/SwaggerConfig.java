c80ad0d0a61592904198023748c60a0cedd7b5c3
duyna5 <duyna5@vng.com.vn>
2020-02-20 14:58:53 +0700

    test manual deploy pipeline

--- htcc-gateway-service/src/main/java/htcc/gateway/service/config/SwaggerConfig.java
+++ htcc-gateway-service/src/main/java/htcc/gateway/service/config/SwaggerConfig.java
@@ -1,8 +1,5 @@
 package htcc.gateway.service.config;
 
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -12,25 +9,20 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 
-import java.io.FileReader;
 import java.io.IOException;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() throws IOException, XmlPullParserException, XmlPullParserException {
-        MavenXpp3Reader reader = new MavenXpp3Reader();
-
-        Model model = reader.read(new FileReader("pom.xml"));
-
+    public Docket api() throws IOException {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("htcc.gateway.service.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(new ApiInfo("HTCC Api Documentation",
                         "",
-                        model.getParent().getVersion(),
+                        "1.0",
                         null,
                         "1612145",
                         null,
