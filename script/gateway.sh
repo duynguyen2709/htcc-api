9c12ed0614397a7c161ff2c097e5b8c02350f713
duyna5 <duyna5@vng.com.vn>
2020-02-26 21:37:25 +0700

    [gateway] test security

--- script/gateway.sh
+++ script/gateway.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker container rm -f htcc-gateway-service
 sleep 2
+docker pull duyna5/htcc-gateway-service:latest
 docker run -p 8761:8761 -d \
         -v "$(pwd)/resources":/conf:ro \
         --add-host=eureka2:10.25.96.4 --add-host=eureka1:172.17.0.1 \
