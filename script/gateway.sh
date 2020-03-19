ab84d7315a8e409c8268fe4ce3b3d2a28d85b5b8
duyna5 <duyna5@vng.com.vn>
2020-03-19 23:01:41 +0700

    init script log.sh

--- script/gateway.sh
+++ script/gateway.sh
@@ -4,9 +4,10 @@ sleep 2
 docker pull duyna5/htcc-gateway-service:latest
 docker run -p 8761:8761 -d \
         -v "$(pwd)/resources":/conf:ro \
+        --memory 256m --memory-swap 512m \
         --add-host=eureka2:10.25.96.4 --add-host=eureka1:127.0.0.1 \
         --name htcc-gateway-service \
         --network="host" \
         -e "TZ=Asia/Ho_Chi_Minh" \
-        -e "JAVA_OPTS=-Xmx128m -Dspring.profiles.active=production-1 -Dspring.config.location=file:/conf/" \
+        -e "JAVA_OPTS=-Dspring.profiles.active=production-1 -Dspring.config.location=file:/conf/" \
         duyna5/htcc-gateway-service
