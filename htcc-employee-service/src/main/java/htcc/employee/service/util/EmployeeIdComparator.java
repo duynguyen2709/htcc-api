65bdbebf66020dd0ed7266627dd185b856848bad
duyna5 <duyna5@vng.com.vn>
2020-04-04 12:21:34 +0700

    [employee] API GetListContact

---
+++ htcc-employee-service/src/main/java/htcc/employee/service/util/EmployeeIdComparator.java
@@ -0,0 +1,19 @@
+package htcc.employee.service.util;
+
+import htcc.common.entity.jpa.EmployeeInfo;
+
+import java.util.Comparator;
+
+public class EmployeeIdComparator implements Comparator<EmployeeInfo> {
+
+    @Override
+    public int compare(EmployeeInfo o1, EmployeeInfo o2) {
+        String id1 = o1.getEmployeeId().trim().substring(o1.getEmployeeId().trim().length() - 5);
+        String id2 = o2.getEmployeeId().trim().substring(o2.getEmployeeId().trim().length() - 5);
+
+        int idNum1 = Integer.parseInt(id1);
+        int idNum2 = Integer.parseInt(id2);
+
+        return Integer.compare(idNum1, idNum2);
+    }
+}
