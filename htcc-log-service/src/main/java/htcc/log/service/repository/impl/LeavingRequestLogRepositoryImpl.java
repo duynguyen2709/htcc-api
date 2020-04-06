1e9dfec0b2d873d0c3030a2614bdfa56560e9239
duyna5 <duyna5@vng.com.vn>
2020-04-06 20:17:37 +0700

    [log] fix bug leak connection datasource

--- htcc-log-service/src/main/java/htcc/log/service/repository/impl/LeavingRequestLogRepositoryImpl.java
+++ htcc-log-service/src/main/java/htcc/log/service/repository/impl/LeavingRequestLogRepositoryImpl.java
@@ -1,54 +1,52 @@
 package htcc.log.service.repository.impl;
 
+import com.zaxxer.hikari.HikariDataSource;
 import htcc.common.entity.log.LeavingRequestLogEntity;
 import htcc.log.service.mapper.LeavingRequestLogRowMapper;
 import htcc.log.service.repository.LeavingRequestLogRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.JdbcUtils;
-import org.springframework.jdbc.support.MetaDataAccessException;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-@Repository
-@Log4j2
-public class LeavingRequestLogRepositoryImpl implements LeavingRequestLogRepository {
+@Repository @Log4j2 public class LeavingRequestLogRepositoryImpl implements LeavingRequestLogRepository {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
-    private DataSource dataSource;
+    private HikariDataSource dataSource;
 
     @Override
     public List<LeavingRequestLogEntity> getLeavingRequestLog(String companyId, String username, String year) {
-        List<LeavingRequestLogEntity> result = new ArrayList<>();
-        List<String> tableNames = getListTableLeavingRequestLog(year);
+        List<LeavingRequestLogEntity> result     = new ArrayList<>();
+        List<String>                  tableNames = getListTableLeavingRequestLog(year);
         for (String table : tableNames) {
             try {
-                String query = String.format("SELECT * FROM %s WHERE companyId='%s' AND username='%s'",
-                                        table, companyId, username);
+                String query =
+                        String.format("SELECT * FROM %s WHERE companyId='%s' AND username='%s'", table, companyId,
+                                username);
                 result.addAll(jdbcTemplate.query(query, new LeavingRequestLogRowMapper()));
-            } catch (Exception e){
+            } catch (Exception e) {
                 log.error("[getLeavingRequestLog] [{}-{}-{}]", table, companyId, username, e);
             }
         }
         return result;
     }
 
-    private List<String> getListTableLeavingRequestLog(String year){
+    private List<String> getListTableLeavingRequestLog(String year) {
         List<String> result = new ArrayList<>();
-        try {
-            DatabaseMetaData md = dataSource.getConnection().getMetaData();
+        try (Connection conn = dataSource.getConnection()) {
+            DatabaseMetaData md = conn.getMetaData();
             ResultSet rs = md.getTables(null, null,
                     "LeavingRequestLog" + year + "%", null);
+
             while (rs.next()) {
                 result.add(rs.getString("TABLE_NAME"));
             }
