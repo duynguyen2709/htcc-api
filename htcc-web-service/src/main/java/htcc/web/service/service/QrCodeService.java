b34a1f7303a57de08ec8d52f343817e2958fb0e2
duyna5 <duyna5@vng.com.vn>
2020-05-24 10:56:42 +0700

    [web] fix error 403 https resttemplate

--- htcc-web-service/src/main/java/htcc/web/service/service/QrCodeService.java
+++ htcc-web-service/src/main/java/htcc/web/service/service/QrCodeService.java
@@ -6,7 +6,12 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import htcc.web.service.config.QrCodeConfig;
 import htcc.web.service.entity.BaseResponse;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,12 +19,14 @@ import java.awt.image.BufferedImage;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Service
 public class QrCodeService {
-
-    @Autowired
-    private RestTemplate restTemplate;
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     @Autowired
     private QrCodeConfig qrCodeConfig;
@@ -31,6 +38,23 @@ public class QrCodeService {
         final String url = String.format("%s%s?companyId=%s&officeId=%s&sig=%s",
                 qrCodeConfig.getBaseUrl(), qrCodeConfig.getMethodName(), companyId, officeId, sig);
 
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        headers.add("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36");
+//        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+//
+//        ResponseEntity<BaseResponse> respEntity = restTemplate
+//                .exchange(url, HttpMethod.GET, entity, BaseResponse.class);
+//        return respEntity.getBody();
+
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
         return restTemplate.getForObject(url, BaseResponse.class);
     }
 
