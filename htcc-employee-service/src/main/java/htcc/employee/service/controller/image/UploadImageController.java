02207f3ced61b4d559b7d2be97bb97b6c1f35d55
duyna5 <duyna5@vng.com.vn>
2020-08-11 20:49:44 +0700

    add api upload array of images

--- htcc-employee-service/src/main/java/htcc/employee/service/controller/image/UploadImageController.java
+++ htcc-employee-service/src/main/java/htcc/employee/service/controller/image/UploadImageController.java
@@ -6,6 +6,7 @@ import htcc.common.util.StringUtil;
 import htcc.employee.service.service.googledrive.GoogleDriveService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Log4j2
@@ -22,7 +25,27 @@ public class UploadImageController {
     @Autowired
     private GoogleDriveService driveService;
 
-    @PostMapping("/public/upload")
+    @PostMapping(value = "/public/uploads", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public List<String> uploadImages(@RequestParam(name = "images", required = false) MultipartFile[] images,
+                                     HttpServletRequest httpServletRequest) throws Exception {
+        long requestTime = System.currentTimeMillis();
+        List<String> response = new ArrayList<>();
+        try {
+            for (MultipartFile image : images) {
+                String fileName = String.format("%s", System.currentTimeMillis());
+                String link     = driveService.uploadAvatar(image, fileName);
+                if (link == null) {
+                    throw new Exception("driveService.uploadAvatar return null");
+                }
+                response.add(link);
+            }
+        } finally {
+            printRequestLogEntity(httpServletRequest, response, requestTime);
+        }
+        return response;
+    }
+
+    @PostMapping(value = "/public/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public String uploadImage(@RequestParam(name = "image", required = false) MultipartFile image,
                               HttpServletRequest httpServletRequest) {
         long requestTime = System.currentTimeMillis();
@@ -43,7 +66,7 @@ public class UploadImageController {
         return response;
     }
 
-    private void printRequestLogEntity(HttpServletRequest request, String response, long requestTime){
+    private void printRequestLogEntity(HttpServletRequest request, Object response, long requestTime){
         RequestLogEntity logEnt = new RequestLogEntity();
         try {
             logEnt.setRequestTime(requestTime);
